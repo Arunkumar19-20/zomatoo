b6c2492964fc00adea7ca482f8ee69002e277d0e
DArunKumar001 <arunkumard.eai24@rathinam.in>
2026-07-02 20:33:30 +0530

    feat: set Chrome User-Agent in WebViewController to avoid Google disallowed_useragent block

--- lib/Services/google_login_page.dart
+++ lib/Services/google_login_page.dart
@@ -52,6 +52,7 @@ class _GoogleLoginPageState extends State<GoogleLoginPage> {
 
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent('Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Mobile Safari/537.36')
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageStarted: (_) => setState(() => _loading = true),
